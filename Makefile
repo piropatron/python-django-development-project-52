b4933d34625dbed5b057696284c3a908fbaa9ccc
piropatron <piropatron1@yandex.ru>
2026-08-16 17:18:28 +0300

    add database

--- Makefile
+++ Makefile
@@ -7,3 +7,35 @@ build:
 .PHONY: render-start
 render-start:
 	gunicorn task_manager.wsgi
+	
+.PHONY: test
+test:
+	uv run pytest
+
+.PHONY: setup
+setup: db-clean install migrate
+
+.PHONY: install
+install:
+	@uv sync
+
+.PHONY: db-clean
+db-clean:
+	@rm db.sqlite3 || true
+
+.PHONY: migrate
+migrate:
+	@$(MANAGE) migrate
+
+.PHONY: shell
+shell:
+	@$(MANAGE) shell_plus --ipython
+
+.PHONY: run
+run:
+	uv run manage.py runserver
+
+.PHONY: lint
+lint:
+	uv run ruff check task_manager
+
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -27,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_ENABLED", False)
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
     os.getenv("ALLOWED_HOST", '')
 ]
 
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
